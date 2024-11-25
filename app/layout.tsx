647943607583ed079d1167373afc365395d652ca
landon-hansley <158092059+landon-hansley@users.noreply.github.com>
2024-11-25 15:34:37 -0500

    eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import { Lora, Roboto_Slab } from 'next/font/google'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Image from 'next/image'
 
 const lora = Lora({ 
   subsets: ['latin'],
